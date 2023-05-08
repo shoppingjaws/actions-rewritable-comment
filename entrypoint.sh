9e019894d9cc7cc39a060a3d2bd383eb8d75bb3e
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-05-08 21:34:22 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 env
 ls /app
 time=$(date)
-# go run src/main.go -owner nakamuloud -commentId default -body Hello -issueNumber 1 -repository actions-dashboard-comment -key abc
 go run src/main.go -owner nakamuloud -commentId default -issueNumber 1 -repository actions-dashboard-comment -key abcd -body Goodbye
