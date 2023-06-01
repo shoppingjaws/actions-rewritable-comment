cbc5316a329a7dea1324f313ef609555f69a5c29
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-06-01 13:42:38 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,4 @@
 env
 cd /app
 
-if [ "${$GITHUB_EVENT_NAME}" == "pull_request" ];
-then
-  echo "Pull Request"
-  NUMBER=$(echo $GITHUB_REF | sed -e 's/[^0-9]//g')
-else
-  NUMBER=${INPUT_ISSUE_NUMBER}
-fi
-
-go run src/main.go -owner ${GITHUB_REPOSITORY_OWNER} -repository ${GITHUB_REPOSITORY#*/} -commentId rewritable-comment -issueNumber ${NUMBER} -key ${KEY:-default} -value ${BODY}
+go run src/main.go -owner ${GITHUB_REPOSITORY_OWNER} -repository ${GITHUB_REPOSITORY#*/} -commentId rewritable-comment -issueNumber ${INPUT_ISSUE_NUMBER} -key ${KEY:-default} -value ${BODY}
