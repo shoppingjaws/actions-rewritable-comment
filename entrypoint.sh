59b7e8ecccac19dd7fca974e09c85d6701bae70d
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-06-01 14:06:11 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,18 @@
 env
 cd /app
 
-go run src/main.go -owner ${GITHUB_REPOSITORY_OWNER} -repository ${GITHUB_REPOSITORY#*/} -commentId rewritable-comment -issueNumber ${INPUT_ISSUE_NUMBER} -key ${KEY:-default} -value ${BODY}
+if [ -z "$INPUT_REPOSITORY" ]; then
+  INPUT_OWNER=${GITHUB_REPOSITORY_OWNER}
+fi
+
+if [ -z "$INPUT_OWNER" ]; then
+  INPUT_REPOSITORY=${GITHUB_REPOSITORY#*/}
+fi
+
+if [ -z "$INPUT_ISSUE_NUMBER" ]; then
+  INPUT_ISSUE_NUMBER=${GITHUB_REF%/*}
+fi
+
+echo env | grep INPUT
+
+go run src/main.go -owner ${INPUT_OWNER} -repository ${INPUT_REPOSITORY} -commentId rewritable-comment -issueNumber ${INPUT_ISSUE_NUMBER} -key ${INPUT_KEY:-default} -value ${INPUT_VALUE}
