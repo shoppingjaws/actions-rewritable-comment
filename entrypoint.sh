5570ee0b6bd3f14c9fb70feb8ae27319380e8b28
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-06-01 14:53:49 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,13 @@ fi
 
 if [ -z "$ISSUE_NUMBER" ]; then
   ISSUE_NUMBER=${GITHUB_REF%/*}
+  ISSUE_NUMBER=${ISSUE_NUMBER##*/}
 fi
 
 echo OWNER: "${OWNER}"
 echo REPOSITORY: "${REPOSITORY}"
 echo ISSUE_NUMBER: "${ISSUE_NUMBER}"
+echo "<!-- KEY : COMMENT_ID --> : " "<!-- ${KEY} : ${COMMENT_ID} -->"
+echo VALUE: "${VALUE}"
 
 go run src/main.go -owner ${OWNER} -repository ${REPOSITORY} -commentId rewritable-comment -issueNumber ${ISSUE_NUMBER} -key ${KEY:-default} -value ${VALUE}
