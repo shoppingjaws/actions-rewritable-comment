31218f1a52c11ce2e3b1ada205d57b931536b922
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-06-01 14:35:56 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,20 @@
 env
 cd /app
 
-if [ -z "$INPUT_OWNER" ]; then
-  INPUT_OWNER=${GITHUB_REPOSITORY_OWNER}
+if [ -z "$OWNER" ]; then
+  OWNER=${GITHUB_REPOSITORY_OWNER}
 fi
 
-if [ -z "$INPUT_REPOSITORY" ]; then
-  INPUT_REPOSITORY=${GITHUB_REPOSITORY#*/}
+if [ -z "$REPOSITORY" ]; then
+  REPOSITORY=${GITHUB_REPOSITORY#*/}
 fi
 
-if [ -z "$INPUT_ISSUE_NUMBER" ]; then
-  INPUT_ISSUE_NUMBER=${GITHUB_REF%/*}
+if [ -z "$ISSUE_NUMBER" ]; then
+  ISSUE_NUMBER=${GITHUB_REF%/*}
 fi
 
-echo INPUT_OWNER: "${INPUT_OWNER}"
-echo INPUT_REPOSITORY: "${INPUT_REPOSITORY}"
-echo INPUT_ISSUE_NUMBER: "${INPUT_ISSUE_NUMBER}"
+echo OWNER: "${OWNER}"
+echo REPOSITORY: "${REPOSITORY}"
+echo ISSUE_NUMBER: "${ISSUE_NUMBER}"
 
-go run src/main.go -owner ${INPUT_OWNER} -repository ${INPUT_REPOSITORY} -commentId rewritable-comment -issueNumber ${INPUT_ISSUE_NUMBER} -key ${INPUT_KEY:-default} -value ${INPUT_VALUE}
+go run src/main.go -owner ${OWNER} -repository ${REPOSITORY} -commentId rewritable-comment -issueNumber ${ISSUE_NUMBER} -key ${KEY:-default} -value ${VALUE}
