cbbab16e26b5051275869a1a65d9b010d52fa0e7
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2023-06-01 13:28:04 +0900

    fix

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ type metadata struct {
 	owner       *string
 	repository  *string
 	issueNumber *int
-	body        *string
+	value        *string
 	commentId   *string
 	key         *string
 }
@@ -32,7 +32,6 @@ func getClient(ctx context.Context) (*github.Client, error) {
 
 // Get adc-comment list
 func getAdcComment(ctx context.Context, client *github.Client, metadata metadata, page int) (*github.IssueComment, int, error) {
-	print("getAdcComment", page, metadata.commentId, "\n")
 	r, _ := regexp.Compile("<!-- " + *metadata.key + ":" + *metadata.commentId + " -->")
 	opt := &github.IssueListCommentsOptions{
 		ListOptions: github.ListOptions{
@@ -41,7 +40,7 @@ func getAdcComment(ctx context.Context, client *github.Client, metadata metadata
 		},
 	}
 
-	cmts, res, err := client.Issues.ListComments(ctx, *metadata.owner, *metadata.repository, 1, opt)
+	cmts, res, err := client.Issues.ListComments(ctx, *metadata.owner, *metadata.repository, *metadata.issueNumber, opt)
 	if err != nil {
 		panic(err)
 	}
@@ -60,16 +59,17 @@ func main() {
 	var metadata metadata = metadata{
 		owner:       flag.String("owner", "", "target repository owner"),
 		commentId:   flag.String("commentId", "", "rewritable comment id"),
-		body:        flag.String("body", "", "rewritable comment body"),
+		value:        flag.String("value", "", "rewritable comment value"),
 		issueNumber: flag.Int("issueNumber", 0, "rewritable comment Issue / PR number"),
 		repository:  flag.String("repository", "", "target repository name"),
 		key:         flag.String("key", "actions-dashboard-comment", "rewritable comment key"),
 	}
 	flag.Parse()
-	if *metadata.owner == "" || *metadata.repository == "" || *metadata.issueNumber == 0 || *metadata.body == "" || *metadata.commentId == "" {
+	if *metadata.owner == "" || *metadata.repository == "" || *metadata.issueNumber == 0 || *metadata.value == "" || *metadata.commentId == "" {
 		fmt.Printf("Error: %v\n", "invalid argument")
 		return
 	}
+	fmt.Printf("owner = %s\ncommentId = %s\nvalue = %s\nissueNumber = %d\nrepository%s\nkey = %s\n", *metadata.owner, *metadata.commentId, *metadata.value, *metadata.issueNumber, *metadata.repository, *metadata.key)
 	ctx := context.Background()
 	client, err := getClient(ctx)
 	if err != nil {
@@ -79,9 +79,18 @@ func main() {
 	c, _, _ := getAdcComment(ctx, client, metadata, 0)
 	if c != nil {
 		// case: Comment Hit
-		client.Issues.EditComment(ctx, *metadata.owner, *metadata.repository, c.GetID(), &github.IssueComment{Body: github.String(*metadata.body + "\n<!-- " + *metadata.key + ":" + *metadata.commentId + " -->")})
+		fmt.Printf("⏪Comment edited from\n" + c.GetBody()+"\n")
+		cmt, _, err := client.Issues.EditComment(ctx, *metadata.owner, *metadata.repository, c.GetID(), &github.IssueComment{Body: github.String(*metadata.value + "\n<!-- " + *metadata.key + ":" + *metadata.commentId + " -->")})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("⏩Comment edited to\n" + cmt.GetBody()+"\n")
 	} else {
 		// case: Comment Not Hit
-		client.Issues.CreateComment(ctx, *metadata.owner, *metadata.repository, *metadata.issueNumber, &github.IssueComment{Body: github.String(*metadata.body + "\n<!-- " + *metadata.key + ":" + *metadata.commentId + " -->")})
+		cmt, _, err := client.Issues.CreateComment(ctx, *metadata.owner, *metadata.repository, *metadata.issueNumber, &github.IssueComment{Body: github.String(*metadata.value + "\n<!-- " + *metadata.key + ":" + *metadata.commentId + " -->")})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("🆕New comment posted\n" + cmt.GetBody()+"\n")
 	}
 }
